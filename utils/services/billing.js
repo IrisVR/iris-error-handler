8b2428c3388d73e4b9322ff46cc1b4bb1e7d587a
Robinnnnn <rokim8@gmail.com>
2017-01-09 15:14:22 -0500

    update cb error

--- utils/services/billing.js
+++ utils/services/billing.js
@@ -4,7 +4,7 @@ const errorTable = require('../../errorTable');
  * Error handling for Chargebee-specific errors
  */
 exports.sendChargebeeError = (res, message) => {
-  const body = errorTable[1010];
+  const body = errorTable[11];
   body.meta.error_message = message;
   res.status(200).send(body);
 };
